04ad867be2d2b9a4baa8a6e3b571dc179ef7e16b
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-05-28 00:27:28 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  // base: '/Taipei-Railway-Station-Homeless/dist/',
-  base: '/Taipei-Railway-Station-Homeless/',
+  base: '/Taipei-Railway-Station-Homeless/dist/',
+  //base: '/Taipei-Railway-Station-Homeless/',
   // base: '/',
 })
